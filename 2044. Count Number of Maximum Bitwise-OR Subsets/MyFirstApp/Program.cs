e7065bcc325964924bba0f6e6519098cc05fdd51
quincyque0 <email@rudometow06.ru>
2025-07-30 10:20:18 +0700

    init

---
+++ 2044. Count Number of Maximum Bitwise-OR Subsets/MyFirstApp/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Solution {
+    private int maxOr = 0;
+    private int count = 0;
+
+    public int CountMaxOrSubsets(int[] nums) {
+        maxOr = 0;
+        count = 0;
+
+        // Сначала определим максимум
+        foreach (int num in nums) {
+            maxOr |= num;
+        }
+
+        // Запускаем DFS
+        Dfs(nums, 0, 0);
+
+        return count;
+    }
+
+    private void Dfs(int[] nums, int index, int currentOr) {
+        if (index == nums.Length) {
+            // Пропускаем пустое множество
+            if (currentOr == maxOr) {
+                count++;
+            }
+            return;
+        }
+
+        // Включить nums[index] в подмножество
+        Dfs(nums, index + 1, currentOr | nums[index]);
+
+        // Не включать nums[index]
+        Dfs(nums, index + 1, currentOr);
+    }
+}
---
+++ TwoSumSolution/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 17;
+
+        int[] result = TwoSum(nums, target);
+        Console.WriteLine($"[{result[0]}, {result[1]}]");
+    }
+
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> numToIndex = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (numToIndex.ContainsKey(complement))
+            {
+                return new int[] { numToIndex[complement], i };
+            }
+            numToIndex[nums[i]] = i;
+        }
+
+        throw new ArgumentException("No solution found");
+    }
+}
